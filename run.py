a38dbac724659adc08a9edbed2d899531425103e
petitmi <petitmi001@gmail.com>
2026-01-25 00:07:24 -0800

    store

--- run.py
+++ run.py
@@ -5,15 +5,5 @@ import os
 
 app = create_app()
 
-# Email config
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
-
-mail = Mail(app)
-init_mail(mail)
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, port=5001)
